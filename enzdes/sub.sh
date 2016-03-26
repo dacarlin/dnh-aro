db738bbf83f77e0c57f215ebe14a1435fe453623
Alex Carlin <carlin@ucdavis.edu>
2016-03-26 15:15:49 -0700

    Fully working and clean version

--- enzdes/sub.sh
+++ enzdes/sub.sh
@@ -1,6 +1,12 @@
-#!/bin/sh
-#SBATCH --output=logs/slurm-%j.out 
-#SBATCH --job-name=dnh_enzdes
+#!/bin/bash
+# 
+#SBATCH --output=logs/slurm-%A_%a.out 
+#SBATCH --error=logs/slurm-%A_%a.err
+#SBATCH --job-name=design 
+#SBATCH -c 1 
+#SBATCH -t 0-4:00 
+#SBATCH --mem-per-cpu 4000  
 
-S=$( sed -n "$SLURM_ARRAY_TASK_ID p" list )
-/share/work/rosetta/source/bin/rosetta_scripts.linuxgccrelease @flags $S 
+S=$( head -$SLURM_ARRAY_TASK_ID list | tail -n 1 )
+/share/work/rosetta/source/bin/rosetta_scripts.default.linuxgccrelease @flags $S 
+
--- match/sub.sh
+++ match/sub.sh
@@ -1,6 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 # 
-#SBATCH --output=logs/slurm-%j.out
-#SBATCH --job-name=dnh_match
+#SBATCH --output=logs/slurm-%A_%a.out
+#SBATCH --error=logs/slurm-%A_%a.err
+#SBATCH --job-name=match
+#SBATCH -n 1
+#SBATCH -t 0-4:00 
+#SBATCH --mem-per-cpu 16000
 
+#secondary matcher 
+#/share/work/rosetta/source/bin/match.linuxgccrelease @flags2 $( sed -n "$SLURM_ARRAY_TASK_ID p" list ) 
+
+#classical matcher 
 /share/work/rosetta/source/bin/match.linuxgccrelease @flags $( sed -n "$SLURM_ARRAY_TASK_ID p" list ) 
