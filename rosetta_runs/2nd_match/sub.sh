ff9dc4f5c7a966294d0dc6908138324a6cfd7e17
Alex Carlin <carlin@ucdavis.edu>
2016-06-29 21:12:42 -0700

    Set up secondary match run

--- rosetta_runs/2nd_match/sub.sh
+++ rosetta_runs/2nd_match/sub.sh
@@ -7,8 +7,6 @@
 #SBATCH -t 0-4:00 
 #SBATCH --mem-per-cpu 16000
 
-#secondary matcher 
-/share/work/rosetta/source/bin/match.linuxgccrelease @flags2 $( sed -n "$SLURM_ARRAY_TASK_ID p" list ) 
+module load rosetta 
+match.linuxgccrelease @flags $( sed -n "$SLURM_ARRAY_TASK_ID p" list ) 
 
-#classical matcher 
-#/share/work/rosetta/source/bin/match.linuxgccrelease @flags $( sed -n "$SLURM_ARRAY_TASK_ID p" list ) 
