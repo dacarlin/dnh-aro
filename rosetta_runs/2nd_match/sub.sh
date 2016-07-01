636301550e7d9c0cdb1db6bc993f218ae394dcca
Alex Carlin <dacarlin@ucdavis.edu>
2016-07-01 12:55:32 -0700

    Updating secondary match

--- rosetta_runs/2nd_match/sub.sh
+++ rosetta_runs/2nd_match/sub.sh
@@ -5,7 +5,8 @@
 #SBATCH --job-name=match
 #SBATCH -n 1
 #SBATCH -t 0-4:00 
-#SBATCH --mem-per-cpu 16000
+#SBATCH --mem-per-cpu 32000
+#SBATCH --array=1-207 
 
 module load rosetta 
 match.linuxgccrelease @flags $( sed -n "$SLURM_ARRAY_TASK_ID p" list ) 
