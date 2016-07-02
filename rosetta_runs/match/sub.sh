6de86fd309424f48eb49c30875f1b73b4b93fe19
Alex Carlin <dacarlin@ucdavis.edu>
2016-07-01 18:09:34 -0700

    Changes to classical matcher with strict natural theozyme definition

--- rosetta_runs/match/sub.sh
+++ rosetta_runs/match/sub.sh
@@ -5,10 +5,8 @@
 #SBATCH --job-name=match
 #SBATCH -n 1
 #SBATCH -t 0-4:00 
-#SBATCH --mem-per-cpu 16000
+#SBATCH --mem-per-cpu 32000
+#SBATCH --array=1-207 
 
-#secondary matcher 
-/share/work/rosetta/source/bin/match.linuxgccrelease @flags2 $( sed -n "$SLURM_ARRAY_TASK_ID p" list ) 
-
-#classical matcher 
-#/share/work/rosetta/source/bin/match.linuxgccrelease @flags $( sed -n "$SLURM_ARRAY_TASK_ID p" list ) 
+module load rosetta 
+match.linuxgccrelease @flags $( sed -n "$SLURM_ARRAY_TASK_ID p" list ) 
